9fc10947892adc573099b002b7b7b0939a8cfdf9
benfict <benfict@gmail.com>
2025-09-22 13:07:25 +0300

    add str and  class Meta with constraints in Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -17,3 +17,25 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings"
     )
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(expected_return_date__gte=models.F("borrow_date")),
+                name="expected_after_borrow",
+            ),
+            models.CheckConstraint(
+                check=(
+                    models.Q(actual_return_date__isnull=True) |
+                    models.Q(actual_return_date__gte=models.F("borrow_date"))
+                ),
+                name="actual_after_borrow",
+            ),
+            models.UniqueConstraint(
+                fields=["book", "user"],
+                condition=models.Q(actual_return_date__isnull=True),
+                name="unique_active_borrowing"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} borrowed {self.book} on {self.borrow_date}"
