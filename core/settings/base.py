3cfd3bbd54af644f126a7036c6b7513a3905a097
Pavlo Semenikhin <pashock1337@gmail.com>
2025-09-22 15:59:21 +0300

    feat(users): configured SimpleJWT in settings.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "borrowings",
     "payments",
     "notifications",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 SPECTACULAR_SETTINGS = {
