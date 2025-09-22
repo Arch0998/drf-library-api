dac561c550563619e5f3be60b89fb2f1e86eaa16
Pavlo Semenikhin <pashock1337@gmail.com>
2025-09-22 13:46:34 +0300

    test(users): test for success user register that returns user without password

--- users/tests/test_serializers.py
+++ users/tests/test_serializers.py
@@ -55,3 +55,17 @@ class UserSerializerTests(APITestCase):
         )
         self.assertFalse(serializer.is_valid())
         self.assertIn("email", serializer.errors)
+
+    def test_register_user_success_returns_user_without_password(self):
+        serializer = RegisterSerializer(data={
+            "email": "user@withoutparol.com",
+            "password": "007BondJames"
+        })
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        user = serializer.save()
+
+        self.assertEqual(user.email, "user@withoutparol.com")
+        self.assertTrue(user.check_password("007BondJames"))
+        self.assertNotIn("password", serializer.data)
+
+
