4759d4429afb740f318267e78ecf7d70463a7181
benfict <benfict@gmail.com>
2025-09-22 18:36:15 +0300

    Add decrement book.inventory atomically

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,5 @@
+from django.db.models import F
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -24,3 +26,8 @@ class BorrowingViewSet(viewsets.ModelViewSet):
                 queryset = queryset.filter(actual_return_date__isnull=False)
         return queryset
 
+    def perform_create(self, serializer):
+        borrowing = serializer.save(user=self.request.user)
+
+        borrowing.book.inventory = F("inventory") - 1
+        borrowing.book.save(update_fields=["inventory"])
