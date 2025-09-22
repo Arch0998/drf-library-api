6816846aecaf18d969f0292f266dca9d32dd8d57
viannik <ivan.nikitski@gmail.com>
2025-09-22 11:16:39 +0200

    enhanced BookAdmin model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import Book
+from books.models import Book
 
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "cover",
+        "inventory",
+        "daily_fee",
+    )
+    search_fields = (
+        "title",
+        "author",
+    )
+    list_filter = (
+        "cover",
+    )
