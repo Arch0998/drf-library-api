9d39d6554233135fe86fc37e18e265e5f020c044
Artcrafterrra <if.artcraft@gmail.com>
2025-09-22 13:26:33 +0300

    Integrate `PaymentViewSet` with URL routing using `SimpleRouter`

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from payments.api import PaymentViewSet
 
 
+router = SimpleRouter()
+router.register(r"payments", PaymentViewSet, basename="payments")
 urlpatterns = [
-    # Define your URL patterns here
+    path("", include(router.urls))
 ]
