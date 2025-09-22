9ad06317a43887502413c403b2189683875f4749
Pavlo Semenikhin <pashock1337@gmail.com>
2025-09-22 13:05:57 +0300

    feat(users): added RegisterSerializer and UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, mixins
 
 
-# Create your views here.
+from users.serializers import UserSerializer, RegisterSerializer
+
+User = get_user_model()
+
+
+class CreateUserViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    serializer_class = RegisterSerializer
+
+
+class ManageUserViewSet(
+    viewsets.GenericViewSet,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin
+):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
