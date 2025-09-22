201e2f849313d3cfe21fb776479f6ea3f990ff06
benfict <benfict@gmail.com>
2025-09-22 18:25:48 +0300

    add BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
 
 
-# Create your views here.
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = [IsAuthenticated]
+
