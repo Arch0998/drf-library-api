5caf979df48256367639a969b9cfc2cc58ec50c5
benfict <benfict@gmail.com>
2025-09-22 18:30:49 +0300

    Add filtering by user_id and is_active in Borrowing queryset

--- borrowings/views.py
+++ borrowings/views.py
@@ -10,3 +10,17 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     serializer_class = BorrowingSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active")
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            else:
+                queryset = queryset.filter(actual_return_date__isnull=False)
+        return queryset
+
