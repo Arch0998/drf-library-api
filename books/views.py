bdca6f8e15bbcf3e431424767faf0b542fdbc3b9
viannik <ivan.nikitski@gmail.com>
2025-09-22 14:12:41 +0200

    implemented BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from books.models import Book
+from books.serializers import BookSerializer
 
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    search_fields = ["title", "author"]
+    filterset_fields = ["cover", "inventory"]
+    ordering_fields = ["title", "author", "inventory", "daily_fee"]
+    ordering = ["title", "author"]
