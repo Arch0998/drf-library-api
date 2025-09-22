a21b4fb7f8491b3811867cfb270d381a8ef052d0
benfict <benfict@gmail.com>
2025-09-22 23:06:42 +0300

    fix test for borrrowings view

--- borrowings/tests/test_views.py
+++ borrowings/tests/test_views.py
@@ -1,10 +1,12 @@
+from datetime import date, timedelta
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from books.models import Book
+
 from users.models import User
+from books.models import Book
 from borrowings.models import Borrowing
-from datetime import date, timedelta
 
 
 class BorrowingViewSetTest(APITestCase):
@@ -21,6 +23,8 @@ class BorrowingViewSetTest(APITestCase):
             inventory=5,
             daily_fee=2.5,
         )
+
+        Borrowing.objects.all().delete()
 
     def test_create_borrowing_decreases_inventory(self):
         url = reverse("borrowing-list")
@@ -42,7 +46,11 @@ class BorrowingViewSetTest(APITestCase):
             "expected_return_date": date.today() + timedelta(days=7),
         }
         response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 204)  # BorrowingOutOfStock
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("book", response.data)
+        self.assertEqual(
+            response.data["book"][0], "This book is out of stock."
+        )
 
     def test_return_book_increases_inventory(self):
         borrowing = Borrowing.objects.create(
@@ -74,16 +82,14 @@ class BorrowingViewSetTest(APITestCase):
         self.assertEqual(response.data["detail"], "Book already returned.")
 
     def test_queryset_filter_user_and_is_active(self):
-        other_user = User.objects.create_user(
-            email="other@example.com", password="123"
-        )
         active_borrowing = Borrowing.objects.create(
             user=self.user,
             book=self.book,
             expected_return_date=date.today() + timedelta(days=7),
         )
-        inactive_borrowing = Borrowing.objects.create(
-            user=other_user,
+
+        Borrowing.objects.create(
+            user=self.user,
             book=self.book,
             expected_return_date=date.today() + timedelta(days=7),
             actual_return_date=date.today(),
@@ -93,6 +99,9 @@ class BorrowingViewSetTest(APITestCase):
         response = self.client.get(
             url, {"user_id": self.user.id, "is_active": "true"}
         )
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["id"], active_borrowing.id)
+        results = response.data["results"]
+
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0]["id"], active_borrowing.id)
