2531df9dd4f557f3f62c7e51e96f00cbf80c8782
Artcrafterrra <if.artcraft@gmail.com>
2025-09-22 13:24:54 +0300

    Add `PaymentViewSet` to handle Payment model API operations

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
 
 
-# Create your views here.
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    """
+    ViewSet for Payment:
+    - list: GET /payments/
+    - create: POST /payments/
+    - retrieve: GET /payments/{id}/
+    """
+    queryset = Payment.objects.all().order_by("-id")
+    serializer_class = PaymentSerializer
