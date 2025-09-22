098a74f0dc18a7b9f35423ba23631fe556c93ffb
viannik <ivan.nikitski@gmail.com>
2025-09-22 15:01:56 +0200

    extended permissions

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 from books.models import Book
 from books.serializers import BookSerializer
 
@@ -7,8 +7,14 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
     search_fields = ["title", "author"]
     filterset_fields = ["cover", "inventory"]
     ordering_fields = ["title", "author", "inventory", "daily_fee"]
     ordering = ["title", "author"]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
