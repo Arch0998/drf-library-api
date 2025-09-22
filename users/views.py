2cb2a24446a7363e400a736932c8a3e73b7e28ec
Pavlo Semenikhin <pashock1337@gmail.com>
2025-09-22 13:12:16 +0300

    feat(users): add permission classes to user viewsets (AllowAny for register, IsAuthenticated for profile)

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets, mixins
-
+from rest_framework import viewsets, mixins, permissions
 
 from users.serializers import UserSerializer, RegisterSerializer
 
@@ -9,6 +8,7 @@ User = get_user_model()
 
 class CreateUserViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 class ManageUserViewSet(
@@ -18,6 +18,7 @@ class ManageUserViewSet(
 ):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
