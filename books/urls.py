73da75f44115e78d0e05beb9a4ce368430242e32
viannik <ivan.nikitski@gmail.com>
2025-09-23 11:12:53 +0200

    updated book router path

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from books.views import BookViewSet
 
 router = DefaultRouter()
-router.register(r"books", BookViewSet)
+router.register(r"", BookViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
