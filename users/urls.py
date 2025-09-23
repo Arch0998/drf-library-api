2bb5fbff99d45c795d5f3ea49ee83fa8048b1ee7
Pavlo Semenikhin <pashock1337@gmail.com>
2025-09-23 03:53:44 +0300

    test(users): add tests for profile view and update (authorized and unauthorized cases)

--- users/urls.py
+++ users/urls.py
@@ -12,12 +12,15 @@ app_name = "users"
 
 router = DefaultRouter()
 router.register("register", CreateUserViewSet, basename="register")
-router.register("me", ManageUserViewSet, basename="me")
+
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path(
+        "me/", ManageUserViewSet.as_view({"get": "retrieve", "put": "update"})
+    ),
 ]
 
 urlpatterns += router.urls
