6d00d9e1c538fb4070ad2657c287101049c6bc4a
Arthur Oleinikov <arch0998@gmail.com>
2025-09-23 14:53:16 +0300

    feat: implement borrowing functionality with serializers, views, and admin integration

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingCreateSerializer,
+    BorrowingDetailSerializer,
+    BorrowingListSerializer,
+    BorrowingSerializer,
+)
 
 
-# Create your views here.
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = (
+        Borrowing.objects.all()
+        .select_related("book", "user")
+        .order_by("-borrow_date")
+    )
+    permission_classes = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["user", "book", "borrow_date", "actual_return_date"]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+        return queryset
