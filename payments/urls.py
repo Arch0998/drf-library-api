3cc7124a22675a2c13e2b38526c1473a6a46d771
Artcrafterrra <if.artcraft@gmail.com>
2025-09-23 14:32:58 +0300

    feat: add success and cancel views for Stripe payments and update settings with Stripe keys

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+
 from payments.views import PaymentViewSet
+from payments import views
 
 
 router = SimpleRouter()
 router.register(r"payments", PaymentViewSet, basename="payments")
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [path("", include(router.urls)),
+    path('success/', views.PaymentSuccessView.as_view(), name='success'),
+    path('cancel/', views.PaymentCancelView.as_view(), name='cancel'),
+]
